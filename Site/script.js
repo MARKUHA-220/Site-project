4dcb0256d048b56f5ec479092b0dce888f3c7869
Ričiard Kozlovskij <markasm133@gmail.com>
2026-01-16 13:57:47 +0200

    Update script.js

--- Site/script.js
+++ Site/script.js
@@ -1,7 +1,7 @@
 'use strict'
 
-var params = {"appName": "graphing", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
-var applet = new GGBApplet(params, true);
-window.addEventListener("load", function() {
-    applet.inject('ggb-element');
-});
+// var params = {"appName": "graphing", "width": 800, "height": 600, "showToolBar": true, "showAlgebraInput": true, "showMenuBar": true };
+// var applet = new GGBApplet(params, true);
+// window.addEventListener("load", function() {
+//     applet.inject('ggb-element');
+// });
